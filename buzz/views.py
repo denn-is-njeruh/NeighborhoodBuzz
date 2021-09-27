4013ac430538a450cdc321fcba57bb779c9998b9
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-28 00:49:49 +0300

    add view function to enable new user registration behavior in the application

--- buzz/views.py
+++ buzz/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate,logout
+from .forms import NewUserForm
+from django.contrib import messages
 
 # Create your views here.
+def register_user(request):
+  if request.method == "POST":
+    form = NewUserForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      messages.success(request, f'Registration Successful.')
+      return redirect('homepage')
+    messages.error(request, f'Unsuccessful registration. Invalid information.')
+  form = NewUserForm()
+  return render(request, 'registration/registration_form.html', {"registration_form": form})
