8ec77652d5d418d50c36148ac42cd71ef6f4101f
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-28 09:13:38 +0300

    add profile form that users can use to update their profiles in the location

--- buzz/forms.py
+++ buzz/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class NewUserForm(UserCreationForm):
   email = forms.EmailField()
@@ -22,3 +23,9 @@ class UpdateUserForm(forms.ModelForm):
   class Meta:
     model = User
     fields = ['username', 'email']
+
+
+class ProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ('image','user','bio','location')
