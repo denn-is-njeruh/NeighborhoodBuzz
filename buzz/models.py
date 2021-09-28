f5bb3bd4c263565e9fdbc89acda07141057c96c4
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-28 09:01:04 +0300

    add the profile model to the application

--- buzz/models.py
+++ buzz/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User,on_delete=models.CASCADE)
+  bio = models.TextField(max_length=200, blank=True)
+  image = CloudinaryField('image', blank=True)
+  location = models.CharField(max_length=80, blank=True)
+
+
+  def __str__(self):
+    return self.user.username
+
+  def save(self):
+    return self.save()
+
+  def delete(self):
+    return self.delete()
