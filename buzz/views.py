185ddc8423c0173d40b88f00ff4f53970cdc7c8e
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-28 08:30:54 +0300

    add login user view function to facilitate login behavior of a user when they use the application

--- buzz/views.py
+++ buzz/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
 from .forms import NewUserForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,22 @@ def register_user(request):
     messages.error(request, f'Unsuccessful registration. Invalid information.')
   form = NewUserForm()
   return render(request, 'registration/registration_form.html', {"registration_form": form})
+
+
+def login_user(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(username=username,password=password)
+      if user is not None:
+        login(request, user)
+        messages.info(request, f"You are now logged in as {username}.")
+        return redirect('homepage')
+      else:
+        messages.error(request, f'Invalid Username or password')
+    else:
+      messages.error(request, f'Invalid username or password')
+  form = AuthenticationForm()
+  return render(request, 'registration/login_form.html', {"login_form": form})
