38771264c00a8e88df008714c97733ebc9634491
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-30 07:28:56 +0300

    add test case for user managers create_user method

--- buzz/tests.py
+++ buzz/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+class UserManagerTest(TestCase):
+  def test_create_user(self):
+    User = get_user_model()
+    user = User.objects.create_user(username='tosh', password='foo')
+    self.assertEqual(user.username, 'tosh')
+    self.assertTrue(user.is_active)
+    self.assertFalse(user.is_staff)
+    self.assertFalse(user.is_superuser)
+    try:
+      self.assertIsNone(user.username)
+    except AttributeError:
+      pass
+    with self.assertRaises(TypeError):
+      User.objects.create_user()
+    with self.assertRaises(TypeError):
+      User.objects.create_user(username='')
+    with self.assertRaises(ValueError):
+      User.objects.create_user(username='', password="foo")
+
+  def test_create_superuser(self):
+    User = get_user_model()
+    admin_user = 
